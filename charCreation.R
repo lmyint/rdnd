d92dd22c1dfe0450c7c250b1975e0169714ac309
Leslie Myint <leslie.myint@gmail.com>
2016-12-18 10:59:09 -0500

    Arg checking, display final stat, invisible return

--- charCreation.R
+++ charCreation.R
@@ -1,13 +1,15 @@
 getStats <- function(method = c("roll", "pointbuy"), disp = TRUE) {
+     method <- match.arg(method)
      if (method=="roll") {
           rolls <- matrix(sample.int(6, size = 24, replace = TRUE), nrow = 6, ncol = 4)
           rollsSorted <- apply(rolls, 1, sort)
           stats <- colSums(rollsSorted[2:4,])
           if (disp) {
+               cat(rep(" ", 8), "Rolls", rep(" ", 8), "Final stat\n", sep = "")
                for (i in 1:6) {
-                    cat("Stat ", i, ": ", rolls[i,1], ", ", rolls[i,2], ", ", rolls[i,3], ", ", rolls[i,4], "\n", sep = "")
+                    cat("Stat ", i, ": ", rolls[i,1], ", ", rolls[i,2], ", ", rolls[i,3], ", ", rolls[i,4], "   ", stats[i], "\n", sep = "")
                }
           }
-          return(stats)
+          invisible(stats)
      }
 }
